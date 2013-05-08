82874ad178088b3b767bac02ac7bb8f4ae6ccad9
Samuel Demirdjian <a@enov.ws>
2013-05-08 11:10:26 +0300

    Move Route lamda function to Aurora_Route

--- classes/aurora/aurora/route.php
+++ classes/aurora/aurora/route.php
@@ -1,5 +1,53 @@
 <?php
 
-class Aurora_Aurora_Route extends Kohana_Route {
+defined('SYSPATH') or die('No direct script access.');
 
+/**
+ * The Aurora Routing class. Defines REST API URL and Reverse Routing
+ *
+ * @package Aurora
+ * @author Samuel Demirdjian
+ * @copyright (c) 2013, Samuel Demirdjian
+ * @license http://license.enov.ws/mit MIT
+ *
+ */
+class Aurora_Aurora_Route
+{
+	/**
+	 * Define routing scheme.
+	 * To be used in init.php of this module
+	 *
+	 * @param string $uri
+	 * @return array/boolean
+	 */
+	public static function route($uri) {
+		$pieces = explode('/', $uri);
+		// if $uri does not start with 'api/' return false
+		$api = array_shift($pieces);
+		if ($api != 'api')
+			return false;
+		// Get the last piece of the uri and test if it contains a numeric ID
+		$last = array_pop($pieces);
+		if (Valid::digit($last)) {
+			$id = $last;
+			$controller = array_pop($pieces);
+		} else {
+			$id = NULL;
+			$controller = $last;
+		}
+		// construct $directory and common_name
+		$directory = $api . ($pieces ? DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $pieces) : '' );
+		// See if exists a Controller of the form
+		// Controller_API_Common_Name
+		if (Kohana::find_file('classes/controller', $directory . DIRECTORY_SEPARATOR . $controller)) {
+			return array(
+				'directory' => $directory,
+				'controller' => $controller,
+				'id' => $id,
+			);
+		} else {
+			// no luck, return false
+			return false;
+		}
+	}
 }
---
+++ classes/aurora/route.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
+
+class Aurora_Route extends Aurora_Aurora_Route
+{
+	
+}
