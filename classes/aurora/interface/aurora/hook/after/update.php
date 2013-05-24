dc2a782cd164331cb589497923ea83f6c9f5656d
Samuel Demirdjian <a@enov.ws>
2013-05-24 22:45:38 +0300

    Interface_Aurora_Hook_After_Update

---
+++ classes/aurora/interface/aurora/hook/after/update.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * after_update hook. If your Aurora implements this interface
+ * the hook will be called after updating Models
+ *
+ * @package Aurora
+ * @author Samuel Demirdjian
+ * @copyright (c) 2013, Samuel Demirdjian
+ * @license http://license.enov.ws/mit MIT
+ *
+ */
+interface Interface_Aurora_Hook_After_Update
+{
+	public function after_update($model);
+}
