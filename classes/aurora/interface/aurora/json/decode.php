ab6c112b14febadadb8d9c7dbf05163df1d75b83
Samuel Demirdjian <a@enov.ws>
2013-05-24 23:38:37 +0300

    JSON encode decode interface skeleton

---
+++ classes/aurora/interface/aurora/json/decode.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * 
+ *
+ * @package Aurora
+ * @author Samuel Demirdjian
+ * @copyright (c) 2013, Samuel Demirdjian
+ * @license http://license.enov.ws/mit MIT
+ *
+ */
+interface Interface_Aurora_JSON_Decode
+{
+
+}
