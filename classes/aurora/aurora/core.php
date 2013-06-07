8c9c261633e727c9a734c278756f3fad9aa91189
Samuel Demirdjian <a@enov.ws>
2013-06-07 09:30:18 +0300

    Implementation of Transactions and Profiler in the Core
    Added try/catch statement
    
    -   to rollback
    -   delete Profiler mark
    -   and throw back the exception again

--- classes/aurora/aurora/core.php
+++ classes/aurora/aurora/core.php
@@ -43,10 +43,25 @@ class Aurora_Aurora_Core
 	 * @return string
 	 */
 	public static function json_encode($object) {
-		// serialize Model or Colleciton into encodeable object
-		$json_obj = static::json_serialize($object);
-		// encode the serialized object
-		return json_encode($json_obj);
+		// start profiling
+		$benchmark = Aurora_Profiler::start($object, __FUNCTION__);
+
+		try {
+
+			// serialize Model or Colleciton into encodeable object
+			$json_obj = static::json_serialize($object);
+			// encode the serialized object
+			$json_str = json_encode($json_obj);
+		} catch (Exception $e) {
+			Aurora_Profiler::delete($benchmark);
+			throw $e;
+		}
+
+		// end profiling
+		Aurora_Profiler::stop($benchmark);
+
+		// return
+		return $json_str;
 	}
 	/**
 	 * JSON decode a JSON string into a Model or a Collection
@@ -55,10 +70,25 @@ class Aurora_Aurora_Core
 	 * @return string
 	 */
 	public static function json_decode($json_str, $type) {
-		// decode the json_str to a stdClass or array
-		$json_obj = json_decode($json_str);
-		// Deserialize decoded object into Model or Colleciton
-		return static::json_deserialize($json_obj, $type);
+		// start profiling
+		$benchmark = Aurora_Profiler::start($type, __FUNCTION__);
+
+		try {
+
+			// decode the json_str to a stdClass or array
+			$json_obj = json_decode($json_str);
+			// Deserialize decoded object into Model or Colleciton
+			$result = static::json_deserialize($json_obj, $type);
+		} catch (Exception $e) {
+			Aurora_Profiler::delete($benchmark);
+			throw $e;
+		}
+
+		// end profiling
+		Aurora_Profiler::stop($benchmark);
+
+		// return
+		return $result;
 	}
 	/**
 	 * Get a json_encodable object from Model or Collection
@@ -78,22 +108,38 @@ class Aurora_Aurora_Core
 	 * @throws InvalidArgumentException if $object is not a model or collection
 	 */
 	public static function json_serialize($object) {
-		// Get the Aurora_ class for this object
-		$au = static::factory($object, 'aurora');
-		// test if it implements json_serialize
-		if ($au instanceof Interface_Aurora_JSON_Serialize)
-			return $au->json_serialize($object);
+		// start profiling
+		$benchmark = Aurora_Profiler::start($object, __FUNCTION__);
 
-		// Set the mode of the serialization
-		if (Aurora_Type::is_model($object)) {
-			$mode = 'from_model';
-		} else if (Aurora_Type::is_collection($object)) {
-			$mode = 'from_collection';
-		} else {
-			throw new InvalidArgumentException("Variable not an instance of Model or Collection");
+		try {
+
+			// Get the Aurora_ class for this object
+			$au = static::factory($object, 'aurora');
+			// test if it implements json_serialize
+			if ($au instanceof Interface_Aurora_JSON_Serialize)
+				return $au->json_serialize($object);
+
+			// Set the mode of the serialization
+			if (Aurora_Type::is_model($object)) {
+				$mode = 'from_model';
+			} else if (Aurora_Type::is_collection($object)) {
+				$mode = 'from_collection';
+			} else {
+				throw new InvalidArgumentException("Variable not an instance of Model or Collection");
+			}
+
+			// Return an stdClass or an array of stdClass
+			$std = Aurora_StdClass::$mode($object);
+		} catch (Exception $e) {
+			Aurora_Profiler::delete($benchmark);
+			throw $e;
 		}
-		// Return an stdClass or an array of stdClass
-		return Aurora_StdClass::$mode($object);
+
+		// end profiling
+		Aurora_Profiler::stop($benchmark);
+
+		// return
+		return $std;
 	}
 	/**
 	 * Convert from JSON to Model or Collection
@@ -110,18 +156,33 @@ class Aurora_Aurora_Core
 	 * @return Model/Collection
 	 */
 	public static function json_deserialize($json, $type) {
-		// Get the Aurora_ class for this object
-		$au = Aurora_Type::is_aurora($type) ? $type : static::factory($type, 'aurora');
-		// test if it implements json_decode
-		if ($au instanceof Interface_Aurora_JSON_Deserialize)
-			return $au->json_deserialize($json);
+		// start profiling
+		$benchmark = Aurora_Profiler::start($type, __FUNCTION__);
 
-		// process: convert json to model or collection
-		return (is_array($json)) ?
-		  // if JSON is array return Collection
-		  Aurora_StdClass::to_collection($json, Aurora_Type::collection($type)) :
-		  // otherwise (if it is of type stdClass) return Model
-		  Aurora_StdClass::to_model($json, Aurora_Type::model($type));
+		try {
+
+			// Get the Aurora_ class for this object
+			$au = Aurora_Type::is_aurora($type) ? $type : static::factory($type, 'aurora');
+			// test if it implements json_decode
+			if ($au instanceof Interface_Aurora_JSON_Deserialize)
+				return $au->json_deserialize($json);
+
+			// process: convert json to model or collection
+			$result = (is_array($json)) ?
+			  // if JSON is array return Collection
+			  Aurora_StdClass::to_collection($json, Aurora_Type::collection($type)) :
+			  // otherwise (if it is of type stdClass) return Model
+			  Aurora_StdClass::to_model($json, Aurora_Type::model($type));
+		} catch (Exception $e) {
+			Aurora_Profiler::delete($benchmark);
+			throw $e;
+		}
+
+		// end profiling
+		Aurora_Profiler::stop($benchmark);
+
+		// return
+		return $result;
 	}
 	/**
 	 * Check if your Model has an ID.
@@ -159,56 +220,71 @@ class Aurora_Aurora_Core
 	 * @return Model/Collection
 	 */
 	public static function load($object, $params = NULL) {
-		// Get the Aurora_ class for this object
-		$au = Aurora_Type::is_aurora($object) ? $object : static::factory($object, 'aurora');
-		// run before hook if exists
-		Aurora_Hook::call($au, 'before_load', $params);
-		// find out mode:
-		// whether this function will load a model or a collection
-		if (is_null($params))
-			$mode = 'collection';
-		if (is_scalar($params))
-			$mode = 'model';
-		if (Aurora_Type::is_model($object))
-			$mode = 'model';
-		if (Aurora_Type::is_collection($object))
-			$mode = 'collection';
-		// Run select query
-		$result = Aurora_Database::select($au, $params);
-		$count = count($result);
+		// start profiling
+		$benchmark = Aurora_Profiler::start($object, __FUNCTION__);
 
-		if (empty($mode)) {
-			$mode = ($count == 1) ? 'model' : 'collection';
-		}
+		try {
 
-		if ($mode == 'model') {
-			if (!$count)
-			// should we Aurora_Hook::call($au, 'after_load', FALSE)? if no result?
-				return false;
-			$model = is_object($object) ? $object : static::factory($object, 'model');
-			$au->db_retrieve($model, $result[0]);
-			Aurora_Hook::call($au, 'after_load', $model);
-			return $model;
-		} else {
-			/* @var $collection Aurora_Collection */
-			$collection = is_object($object) ? $object : static::factory($object, 'collection');
-			$model_name = Aurora_Type::model($object);
-			$array = & $collection->to_array();
-			$row_pkey = Aurora_Database::row_pkey($au);
-			foreach ($result as $row) {
-				$pkey = $row[$row_pkey];
-				if (isset($array[$pkey]))
-					$model = $array[$pkey];
-				else {
-					$model = new $model_name;
-					$array[$pkey] = $model;
-				}
-				$au->db_retrieve($model, $row);
+			// Get the Aurora_ class for this object
+			$au = Aurora_Type::is_aurora($object) ? $object : static::factory($object, 'aurora');
+			// run before hook if exists
+			Aurora_Hook::call($au, 'before_load', $params);
+			// find out mode:
+			// whether this function will load a model or a collection
+			if (is_null($params))
+				$mode = 'collection';
+			if (is_scalar($params))
+				$mode = 'model';
+			if (Aurora_Type::is_model($object))
+				$mode = 'model';
+			if (Aurora_Type::is_collection($object))
+				$mode = 'collection';
+			// Run select query
+			$rowset = Aurora_Database::select($au, $params);
+			$count = count($rowset);
+
+			if (empty($mode)) {
+				$mode = ($count == 1) ? 'model' : 'collection';
 			}
-			// run after hook if exists
-			Aurora_Hook::call($au, 'after_load', $collection);
-			return $collection;
+
+			if ($mode == 'model') {
+				if (!$count)
+				// should we Aurora_Hook::call($au, 'after_load', FALSE)? if no result?
+					return false;
+				$model = is_object($object) ? $object : static::factory($object, 'model');
+				$au->db_retrieve($model, $rowset[0]);
+				Aurora_Hook::call($au, 'after_load', $model);
+				$result = $model;
+			} else {
+				/* @var $collection Aurora_Collection */
+				$collection = is_object($object) ? $object : static::factory($object, 'collection');
+				$model_name = Aurora_Type::model($object);
+				$array = & $collection->to_array();
+				$row_pkey = Aurora_Database::row_pkey($au);
+				foreach ($rowset as $row) {
+					$pkey = $row[$row_pkey];
+					if (isset($array[$pkey]))
+						$model = $array[$pkey];
+					else {
+						$model = new $model_name;
+						$array[$pkey] = $model;
+					}
+					$au->db_retrieve($model, $row);
+				}
+				// run after hook if exists
+				Aurora_Hook::call($au, 'after_load', $collection);
+				$result = $collection;
+			}
+		} catch (Exception $e) {
+			Aurora_Profiler::delete($benchmark);
+			throw $e;
 		}
+
+		// end profiling
+		Aurora_Profiler::stop($benchmark);
+
+		// return
+		return $result;
 	}
 	/**
 	 * Save a model or a collection to the database
@@ -218,28 +294,51 @@ class Aurora_Aurora_Core
 	 * @return Model/Collection
 	 */
 	public static function save($object) {
-		// Get the Aurora_ class for this object
-		$au = static::factory($object, 'aurora');
-		// run before hook if exists
-		Aurora_Hook::call($au, 'before_save', $object);
-		// deep save by looping through the collection
-		if (Aurora_Type::is_collection($object)) {
-			foreach ($object as $model) {
-				static::save($model);
+		// start profiling
+		$benchmark = Aurora_Profiler::start($object, __FUNCTION__);
+
+		try {
+
+			// Get the Aurora_ class for this object
+			$au = static::factory($object, 'aurora');
+			// start transaction
+			Aurora_Database::begin($au);
+			// run before hook if exists
+			Aurora_Hook::call($au, 'before_save', $object);
+			// deep save by looping through the collection
+			if (Aurora_Type::is_collection($object)) {
+				foreach ($object as $model)
+				// run _save for each model
+					static::_save($model, $au);
+			} else {
+				// run _save once
+				static::_save($object, $au);
 			}
-			return $object;
+			// run after hook if exists
+			Aurora_Hook::call($au, 'after_save', $object);
+		} catch (Exception $e) {
+			Aurora_Database::rollback($au);
+			Aurora_Profiler::delete($benchmark);
+			throw $e;
 		}
-		// test if the model is new
-		if (static::is_new($object)) {
-			// if it's new create in the database
-			static::create($object);
-		} else {
-			// if it has an ID update model in database
-			static::update($object);
-		}
-		// run after hook if exists
-		Aurora_Hook::call($au, 'after_save', $object);
+
+		// commit
+		Aurora_Database::commit($au);
+
+		// end profiling
+		Aurora_Profiler::stop($benchmark);
+
+		// return
 		return $object;
+	}
+	protected static function _save($model, $aurora) {
+		return
+		  // test if the model is new
+		  static::is_new($model) ?
+		  // if it's new create in the database
+		  static::create($model, $aurora) :
+		  // if it has an ID update model in database
+		  static::update($model, $aurora);
 	}
 	/**
 	 * Delete a model or a collection from the database
@@ -250,27 +349,51 @@ class Aurora_Aurora_Core
 	 * @throws Kohana_Exception
 	 */
 	public static function delete($object) {
-		// Get the Aurora_ class for this object
-		$au = static::factory($object, 'aurora');
-		// run before hook if exists
-		Aurora_Hook::call($au, 'before_delete', $object);
-		// deep delete by looping through the collection
-		if (Aurora_Type::is_collection($object)) {
-			foreach ($object as $model) {
-				static::delete($model);
+		// start profiling
+		$benchmark = Aurora_Profiler::start($object, __FUNCTION__);
+
+		try {
+			// Get the Aurora_ class for this object
+			$au = static::factory($object, 'aurora');
+			// start transaction
+			Aurora_Database::begin($au);
+			// run before hook if exists
+			Aurora_Hook::call($au, 'before_delete', $object);
+			// deep delete by looping through the collection
+			$IDs = array();
+			if (Aurora_Type::is_collection($object)) {
+				foreach ($object as $model)
+					$IDs[] = static::_delete($model);
+			} else {
+				$IDs[] = static::_delete($model);
 			}
+			// Run the delete query
+			$result = Aurora_Database::delete($au, $IDs);
+			// run after hook if exists
+			Aurora_Hook::call($au, 'after_delete', $object);
+
+			//
+		} catch (Exception $e) {
+			Aurora_Database::rollback($au);
+			Aurora_Profiler::delete($benchmark);
+			throw $e;
 		}
+
+		// commit
+		Aurora_Database::commit($au);
+
+		// end profiling
+		Aurora_Profiler::stop($benchmark);
+
+		// return result
+		return $result;
+	}
+	protected static function _delete($model) {
 		// Test if $model is_new and throw exception if TRUE
 		if (static::is_new($object))
 			throw new Kohana_Exception('Can not delete a new Model.');
 		// Get the value $pk (the ID) of the $model
-		$pk = Aurora_Property::get_pkey($object);
-		// Run the delete query
-		$result = Aurora_Database::delete($au, $pk);
-		// run after hook if exists
-		Aurora_Hook::call($au, 'after_delete', $object);
-		// return result
-		return $result;
+		return Aurora_Property::get_pkey($model);
 	}
 	/**
 	 * Inserts a model to the database
@@ -278,21 +401,19 @@ class Aurora_Aurora_Core
 	 * @param Model $model
 	 * @return Model
 	 */
-	protected static function create($model) {
-		// Get the Aurora_ class for this model
-		$au = static::factory($model, 'aurora');
+	protected static function create($model, $aurora) {
 		// run before hook if exists
-		Aurora_Hook::call($au, 'before_create', $model);
+		Aurora_Hook::call($aurora, 'before_create', $model);
 		// Get the $row array from Aurora_ to be inserted
-		$row = $au->db_persist($model);
+		$row = $aurora->db_persist($model);
 		// Run the insert query
-		$result = Aurora_Database::insert($au, $row);
+		$result = Aurora_Database::insert($aurora, $row);
 		if ($result) {
 			$inserted_id = $result[0];
 			Aurora_Property::set_pkey($model, $inserted_id);
 		}
 		// run after hook if exists
-		Aurora_Hook::call($au, 'after_create', $model);
+		Aurora_Hook::call($aurora, 'after_create', $model);
 		// return $model
 		return $model;
 	}
@@ -302,19 +423,17 @@ class Aurora_Aurora_Core
 	 * @param Model $model
 	 * @return Model
 	 */
-	protected static function update($model) {
-		// Get the Aurora_ class for this model
-		$au = static::factory($model, 'aurora');
+	protected static function update($model, $aurora) {
 		// run before hook if exists
-		Aurora_Hook::call($au, 'before_update', $model);
+		Aurora_Hook::call($aurora, 'before_update', $model);
 		// Get the $row array from Aurora_ to be inserted
-		$row = $au->db_persist($model);
+		$row = $aurora->db_persist($model);
 		// Get the value $pk (the ID) of the $model
 		$pk = Aurora_Property::get_pkey($model);
 		// Run the update query
-		Aurora_Database::update($au, $row, $pk);
+		Aurora_Database::update($aurora, $row, $pk);
 		// run after hook if exists
-		Aurora_Hook::call($au, 'after_update', $model);
+		Aurora_Hook::call($aurora, 'after_update', $model);
 		// return $model
 		return $model;
 	}
