0f46fd219ff635d935e0a599ee23784a71acf8f6
Samuel Demirdjian <a@enov.ws>
2013-07-23 10:47:42 +0300

    Aurora_Hook: some arguments need to be passed by reference

--- classes/aurora/aurora/hook.php
+++ classes/aurora/aurora/hook.php
@@ -2,10 +2,10 @@
 
 class Aurora_Aurora_Hook
 {
-	public static function call($aurora, $hook, $args) {
+	public static function call($aurora, $hook, &$args) {
 		$interface = "Interface_Aurora_Hook_" . $hook;
 		if ($aurora instanceof $interface) {
-			call_user_func_array(array($aurora, $hook), $args);
+			call_user_func_array(array($aurora, $hook), array(&$args));
 		}
 	}
 }
