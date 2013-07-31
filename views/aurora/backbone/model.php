3eec39876cf55c246001b1f13603f1c2e2f9c50e
Samuel Demirdjian <a@enov.ws>
2013-07-31 09:15:32 +0300

    delete backbone code generation views

--- views/aurora/backbone/model.php
+++
@@ -1,9 +0,0 @@
-<script>
-
-
-var <?= Aurora_Type::classname($model) ?> = Backbone.Model.extend({
-	defaults: <?= AU::json_encode($model)?>,
-	urlRoot: '<?= Aurora_API::url($model)?>',
-});
-
-</script>
