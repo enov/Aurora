cbb520a40b6285c9082f4e97243d2219edad3e49
Samuel Demirdjian <a@enov.ws>
2014-03-12 10:45:23 +0200

    Add Interface_Aurora_JSON combining serialize and deserialize

---
+++ classes/Interface/Aurora/JSON.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Interface for custom JSON serialization and deserialization.
+ *
+ * @package Aurora
+ * @category Interfaces
+ * @author Samuel Demirdjian
+ * @copyright (c) 2013, Samuel Demirdjian
+ * @license http://enov.mit-license.org MIT
+ *
+ */
+interface Interface_Aurora_JSON extends Interface_Aurora_JSON_Serialize,
+ Interface_Aurora_JSON_Deserialize
+{
+
+}
